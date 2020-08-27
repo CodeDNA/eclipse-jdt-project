86a20568e1a4a6be587272a75a4ce5747e9bfe7d
Kia Teymourian <kiat@bu.edu>
2020-08-27 17:58:51 -0400

    update

--- JavaProjectTemplate/src/main/java/edu/bu/met/cs665/Main.java
+++ JavaProjectTemplate/src/main/java/edu/bu/met/cs665/Main.java
@@ -45,3 +45,45 @@ public class Main {
     return stringBuilder.toString();
   }
 } // ** Main Class
+
+
+
+class VariablePrinter {
+  // ! ERROR IN THIS METHOD || Import error **********************************
+   public static ASTNode getASTNode(String codeFragment) {
+  
+	ASTParser parser = ASTParser.newParser(AST.JLS8);
+    parser.setKind(ASTParser.K_COMPILATION_UNIT);
+    parser.setSource(codeFragment.toCharArray());
+    parser.setResolveBindings(false);
+
+    ASTNode node = null;
+    try {
+      node = (CompilationUnit) parser.createAST(null);
+    } catch (Exception e) {
+      return null;
+    }
+    return node;
+
+  }
+
+  public static void printVariables(String codeFragment) {
+    ASTNode node = getASTNode(codeFragment);
+    node.accept(new ASTVisitor() {
+
+      int varCount = 0;
+
+      @Override
+      public boolean visit(VariableDeclarationFragment node) {
+        System.out.println(node.getName().toString());
+        return super.visit(node);
+      }
+
+      ;
+    });
+  }
+} //  Variable Printer Class
+
+
+
+ 
